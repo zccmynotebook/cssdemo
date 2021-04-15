bccf0a9c5f43747137e8130b370ca90f6b7ad67b
zhucuicui <zhucuicui@jd.com>
2021-04-15 19:13:26 +0800

    add

--- arithmetic/insert.js
+++ arithmetic/insert.js
@@ -1,6 +1,10 @@
 function insert(list){
     if (!Array.isArray(list)) return
-     
+    let init = list[0] 
+    let arr = []
+    for(let i=1;i<list.length-1;i++){
+       
+    }
     return list
 }
 console.log(insert([12,1,6,17,20]))
