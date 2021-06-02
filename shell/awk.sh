992027176c75e7735833000d606341f93b03f6a5
zhucuicui <zhucuicui@jd.com>
2021-06-02 20:56:09 +0800

    add

---
+++ shell/awk.sh
@@ -0,0 +1,8 @@
+{
+ sum=0;
+for (i=1;i<4;i++){
+  sum+=$i
+}
+ print sum
+}
+#运行： awk -f ./awk.sh ./num 
---
+++ shell/function.sh
@@ -0,0 +1,25 @@
+function a(){
+    echo 'aaa'
+    return 123
+}
+b(){
+echo 'bbb'
+}
+#调用
+a;
+echo $?
+b
+echo $?
+
+
+funWithParam(){
+    echo "第一个参数为 $1 !"
+    echo "第二个参数为 $2 !"
+    echo "第十个参数为 $10 !"
+    echo "第十个参数为 ${10} !"
+    echo "第十一个参数为 ${11} !"
+    echo "参数总数有 $# 个!"
+    echo "作为一个字符串输出所有参数 $* !"
+}
+
+funWithParam 1 2 3 4 5 6 7 8 9 34 73
